0281072d7b78ad11b63b644f5fc95054979dbc44
Clint88 <ccrockett154@west-mec.org>
2021-03-01 13:57:47 -0700

    Started vehicle page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,10 +7,10 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-  // signedin = true;
+  signedin = true;
 
   constructor(public auth: AuthService) {}
   signIn = this.auth.googleSignin();
