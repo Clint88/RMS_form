d14174f1a2e4cc6f1e9d352516814dce29de89af
Zac <zacace@ingoglia.com>
2021-03-10 02:28:40 -0700

    View More is Complete
    Added remaining template html needed for all 3 page types
    Added remaining TS logic to pull all data
    Added error checking to prevent page from being loaded when it's not called by a modal controller

--- src/app/modals/view-more/view-more.page.ts
+++ src/app/modals/view-more/view-more.page.ts
@@ -1,7 +1,16 @@
 // Core+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ModalController, NavParams, AlertController } from '@ionic/angular';
+import { ModalController, NavParams } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+// Models
+import { Person } from 'src/app/models/person.model';
+import { Vehicle } from 'src/app/models/vehicle.model';
+
+// Services
+import { PersonService } from 'src/app/services/person.service';
+import { VehicleService } from 'src/app/services/vehicle.service';
 
 @Component({
   selector: 'app-view-more',
@@ -16,11 +25,18 @@ export class ViewMorePage implements OnInit {
     vehicle: false,
     incident: false,
   };
-  data: object;
+  data;
+
+  // Incident Report Additional Info
+  persons: Observable<Person[]> = this.personService.appendPersons$;
+  vehicles: Observable<Vehicle[]> = this.vehicleService.appendVehicles$;
 
   constructor(
+    private vehicleService: VehicleService,
+    private personService: PersonService,
     private modalController: ModalController,
-    private navParams: NavParams
+    private navParams: NavParams,
+    private router: Router
   ) {
     if (this.navParams.get('type')) {
       this.type = this.navParams.get('type');
@@ -35,5 +51,12 @@ export class ViewMorePage implements OnInit {
     this.modalController.dismiss();
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    if (!this.data) {
+      console.error('Error 05: Page was loaded without Modal Context');
+      this.router.navigate(['/home']);
+    }
+    await this.personService.setLinkedPersons(this.data.persons);
+    await this.vehicleService.setLinkedVehicles(this.data.vehicles);
+  }
 }
