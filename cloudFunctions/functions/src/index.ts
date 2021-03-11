84a2982f1bc79f41e2bec8503764be9b5fd03e99
Zac <zacace@ingoglia.com>
2021-03-11 12:55:50 -0700

    Updated Cloud functions to work with Algolia

--- cloudFunctions/functions/src/index.ts
+++ cloudFunctions/functions/src/index.ts
@@ -1,9 +1,20 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import { deleteDoc, getUserData } from './microFunctions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+const algoliasearch = require("algoliasearch").default;
+import { deleteDoc, getUserData } from "./microFunctions";
 
 admin.initializeApp();
 const auth = admin.auth();
+
+// Initalize Algolia Search
+// App ID and API Key are stored in functions config variables
+const ALGOLIA_ID = functions.config().algolia.app_id;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+
+const searchConfig = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+const personIndex = searchConfig.initIndex("persons");
+const vehicleIndex = searchConfig.initIndex("vehicles");
+const incidentIndex = searchConfig.initIndex("incidents");
 
 interface ClaimsDocumentData extends admin.firestore.DocumentData {
   _lastCommitted?: admin.firestore.Timestamp;
@@ -11,7 +22,7 @@ interface ClaimsDocumentData extends admin.firestore.DocumentData {
 
 // mirrorCustomClaims
 export const mirrorCustomClaims = functions.firestore
-  .document('userClaims/{uid}')
+  .document("userClaims/{uid}")
   .onWrite(async (change, context) => {
     const beforeData: ClaimsDocumentData = change.before.data() || {};
     const afterData: ClaimsDocumentData = change.after.data() || {};
@@ -21,7 +32,7 @@ export const mirrorCustomClaims = functions.firestore
       !beforeData._lastCommitted.isEqual(afterData._lastCommitted);
 
     if (skipUpdate) {
-      console.log('No Changes');
+      console.log("No Changes");
       return;
     }
 
@@ -35,7 +46,7 @@ export const mirrorCustomClaims = functions.firestore
 
     if (stringifyClaims.length > 1000) {
       console.error(
-        'New custom claims object string greater than 1000 chars',
+        "New custom claims object string greater than 1000 chars",
         stringifyClaims
       );
       return;
@@ -45,7 +56,7 @@ export const mirrorCustomClaims = functions.firestore
     console.log(`setting custom claims for ${uid}`, newClaims);
 
     await auth.setCustomUserClaims(uid, newClaims);
-    console.log('Updating document timestamp');
+    console.log("Updating document timestamp");
 
     await change.after.ref.update({
       _lastCommitted: admin.firestore.FieldValue.serverTimestamp(),
@@ -55,25 +66,25 @@ export const mirrorCustomClaims = functions.firestore
 
 // setIsStudent
 export const setIsStudent = functions.firestore
-  .document('users/{uid}')
+  .document("users/{uid}")
   .onCreate(async (change, context) => {
     // User Info
     const uid = context.params.uid;
-    const userDocRef = admin.firestore().collection('users').doc(uid);
+    const userDocRef = admin.firestore().collection("users").doc(uid);
     const userData: any = await getUserData(uid);
     // Email formatting
-    const emailDomain = userData.email.split('@');
+    const emailDomain = userData.email.split("@");
     const emailSuffix = emailDomain[1].toLowerCase();
     // Sets boolean status of constant to true if email address domain is valid, and false if not
     const goodUser: boolean =
-      emailSuffix === 'west-mec.org' || emailSuffix === 'west-mec.edu';
+      emailSuffix === "west-mec.org" || emailSuffix === "west-mec.edu";
     console.log(
       `emailDomain: ${emailDomain}, emailSuffix: ${emailSuffix}, goodUser: ${goodUser}`
     );
 
     // Sets the isStudent claim status for the new user depending on whether the user used a west-mec email
     console.log(`Setting isUser to ${goodUser}`);
-    await admin.firestore().collection('userClaims').doc(uid).set(
+    await admin.firestore().collection("userClaims").doc(uid).set(
       {
         isUser: goodUser,
         isAdmin: false,
@@ -97,19 +108,19 @@ export const setIsStudent = functions.firestore
 
 // userDeletionMonitor
 export const userDeletionMonitor = functions.firestore
-  .document('users/{usersId}')
+  .document("users/{usersId}")
   .onWrite(async (change, context) => {
     const beforeData: ClaimsDocumentData = change.before.data() || {};
     const afterData: ClaimsDocumentData = change.after.data() || {};
 
     const targetUid = beforeData.uid;
 
-    if (!targetUid || targetUid === '') {
+    if (!targetUid || targetUid === "") {
       return;
     }
 
     if (beforeData.uid === afterData.uid) {
-      console.log('No changes detected, exiting');
+      console.log("No changes detected, exiting");
       return;
     }
 
@@ -123,6 +134,107 @@ export const userDeletionMonitor = functions.firestore
         console.log(`Error deleting user: ${targetUid}`, error);
       });
 
-    await deleteDoc('userClaims', targetUid);
+    await deleteDoc("userClaims", targetUid);
     return;
   });
+
+// ---Algolia Search---
+
+// -Persons Collection-
+
+// Add all new person docs to the person search index
+export const addTopersonIndex = functions.firestore
+  .document("persons/{personId}")
+  .onCreate((snapshot) => {
+    // Get the person document
+    const data = snapshot.data();
+    // Add an 'objectID' field which Algolia requires
+    const objectID = snapshot.id;
+
+    // Write to the algolia index
+    return personIndex.saveObject({ ...data, objectID });
+  });
+
+// Update the search index for persons each time that a person doc is updated
+export const updatepersonIndex = functions.firestore
+  .document("persons/{personId}")
+  .onUpdate((change) => {
+    // Get the person document
+    const newData = change.after.data();
+    // Add an 'objectID' field which Algolia requires
+    const objectID = change.after.id;
+
+    // Write to the algolia index
+    return personIndex.saveObject({ ...newData, objectID });
+  });
+
+// Delete persons from the index if their document is removed
+export const deleteFrompersonIndex = functions.firestore
+  .document("persons/{personId}")
+  .onDelete((snapshot) => personIndex.deleteObject(snapshot.id));
+
+// -Vehicles Collection-
+
+// Add all new vehicle docs to the vehicle search index
+export const addTovehicleIndex = functions.firestore
+  .document("vehicles/{vehicleId}")
+  .onCreate((snapshot) => {
+    // Get the vehicle document
+    const data = snapshot.data();
+    // Add an 'objectID' field which Algolia requires
+    const objectID = snapshot.id;
+
+    // Write to the algolia index
+    return vehicleIndex.saveObject({ ...data, objectID });
+  });
+
+// Update the search index for vehicles each time that a vehicle doc is updated
+export const updatevehicleIndex = functions.firestore
+  .document("vehicles/{vehicleId}")
+  .onUpdate((change) => {
+    // Get the vehicle document
+    const newData = change.after.data();
+    // Add an 'objectID' field which Algolia requires
+    const objectID = change.after.id;
+
+    // Write to the algolia index
+    return vehicleIndex.saveObject({ ...newData, objectID });
+  });
+
+// Delete vehicles from the index if their document is removed
+export const deleteFromvehicleIndex = functions.firestore
+  .document("vehicles/{vehicleId}")
+  .onDelete((snapshot) => vehicleIndex.deleteObject(snapshot.id));
+
+// -Incidents Collection-
+
+// Add all new incident docs to the incident search index
+export const addToincidentIndex = functions.firestore
+  .document("incidents/{incidentId}")
+  .onCreate((snapshot) => {
+    // Get the incident document
+    const data = snapshot.data();
+    // Add an 'objectID' field which Algolia requires
+    const objectID = snapshot.id;
+
+    // Write to the algolia index
+    return incidentIndex.saveObject({ ...data, objectID });
+  });
+
+// Update the search index for incidents each time that a incident doc is updated
+export const updateincidentIndex = functions.firestore
+  .document("incidents/{incidentId}")
+  .onUpdate((change) => {
+    // Get the incident document
+    const newData = change.after.data();
+    // Add an 'objectID' field which Algolia requires
+    const objectID = change.after.id;
+
+    // Write to the algolia index
+    return incidentIndex.saveObject({ ...newData, objectID });
+  });
+
+// Delete incidents from the index if their document is removed
+export const deleteFromincidentIndex = functions.firestore
+  .document("incidents/{incidentId}")
+  .onDelete((snapshot) => incidentIndex.deleteObject(snapshot.id));
